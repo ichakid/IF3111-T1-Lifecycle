b29055bdb937c2fbededa14027c42528829cc297
ichakid <choirunnisa.fatima@gmail.com>
2015-02-08 07:26:22 +0700

    Create Project LifeCycle

---
+++ AndroidLifeCycleActivity/app/src/main/java/ichakid/androidlifecycleactivity/TracerActivity.java
@@ -0,0 +1,65 @@
+package ichakid.androidlifecycleactivity;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class TracerActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        notify("onCreate");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        notify("onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        notify("onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        notify("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        notify("onDestroy");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        notify("onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        notify("onSaveInstanceState");
+    }
+
+    private void notify(String methodName) {
+        String name = this.getClass().getName();
+        String[] strings = name.split("\\.");
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle(methodName + " " + strings[strings.length - 1]).setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentText(name).build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify((int) System.currentTimeMillis(), noti);
+    }
+}
