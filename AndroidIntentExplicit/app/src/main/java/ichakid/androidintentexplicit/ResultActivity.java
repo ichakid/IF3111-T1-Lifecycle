c74479aefed4f1f395e0fb2f0d1c19c13e490536
ichakid <choirunnisa.fatima@gmail.com>
2015-02-08 07:26:01 +0700

    Create Activities
    Create class and layout MainActivity;
    Create class and layout ResultActivity;

---
+++ AndroidIntentExplicit/app/src/main/java/ichakid/androidintentexplicit/ResultActivity.java
@@ -0,0 +1,17 @@
+package ichakid.androidintentexplicit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ichakid.androidintentexplicit.R;
+
+/**
+ * Created by User on 2/8/2015.
+ */
+public class ResultActivity extends Activity {
+    @Override
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_result);
+    }
+}
