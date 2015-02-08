f0da03d5968f7897e1e11221b9d19f962a228cf8
ichakid <choirunnisa.fatima@gmail.com>
2015-02-08 08:30:10 +0700

    Finished Ex 11
    Exercise 11: Browser Activity

--- AndroidIntentBrowseFilter/app/src/main/java/ichakid/androidintentbrowsefilter/BrowserActivity.java
+++ AndroidIntentBrowseFilter/app/src/main/java/ichakid/androidintentbrowsefilter/BrowserActivity.java
@@ -1,9 +1,17 @@
 package ichakid.androidintentbrowsefilter;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 
 public class BrowserActivity extends ActionBarActivity {
@@ -11,29 +19,30 @@ public class BrowserActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setContentView(R.layout.activity_browser);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_browser, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Intent intent = getIntent();
+        TextView text = (TextView) findViewById(R.id.textView);
+        String action = intent.getAction();
+        if (!action.equals(Intent.ACTION_VIEW)) {
+            throw new RuntimeException("Should not happen");
         }
+        Uri data = intent.getData();
+        URL url;
+        try {
+            url = new URL(data.getScheme(), data.getHost(), data.getPath());
+            BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                text.append(line);
+            }
 
-        return super.onOptionsItemSelected(item);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
